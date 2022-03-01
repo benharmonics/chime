dabb586be0cdaee295d5a25a2d0c9485167c770f
benharmonics <benjamin.jiron+git@gmail.com>
2022-03-01 01:33:26 -0700

    Initial Commit

---
+++ chime.py
@@ -0,0 +1,37 @@
+from threading import Event
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
+import pygame
+
+exit = Event()
+
+pygame.mixer.init()
+pygame.mixer.music.load('Maj5_ascending.mp3')
+
+
+def main():
+    print('chime.py - Play an alert every X seconds')
+    try:
+        interval = int(input('How many seconds between chimes? '))
+        print('Press Ctrl+C to exit.')
+    except ValueError as e:
+        print(f'Could not interpret input: {e}')
+        exit.set()
+
+    while not exit.is_set():
+        pygame.mixer.music.play()
+        exit.wait(interval)
+    print('\nExiting.')
+
+
+def quit(_signo, _frame):
+    exit.set()
+
+
+if __name__ == '__main__':
+    import signal
+
+    for sig in ('TERM', 'HUP', 'INT'):
+        signal.signal(getattr(signal, 'SIG' + sig), quit)
+
+    main()
